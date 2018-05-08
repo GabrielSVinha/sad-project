0574813b7ad541b0ea25b666f55d3c8fd81c85a0
Gabriel Vinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-05-08 18:03:31 -0300

    Add customer dimension

--- delivery1/star/schema.sql
+++ delivery1/star/schema.sql
@@ -1,1 +1,16 @@
 DROP DATABASE IF EXISTS schema
+
+CREATE TABLE CustomersDimension (
+    CustomerID      int             NOT NULL,
+    CompanyName     varchar(30)     NOT NULL,
+    ContactName     varchar(30)     NOT NULL,
+    ContactTitle    varchar(30)     NOT NULL,
+    Address         varchar(100)    NOT NULL,
+    City            varchar(10)     NOT NULL,
+    Region          int             NOT NULL,
+    PostalCode      int             NOT NULL,
+    Country         varchar(10)     NOT NULL,
+    Phone           varchar(8)      NOT NULL,
+    Fax             int             NOT NULL,
+    PRIMARY KEY (Customer ID)
+)
