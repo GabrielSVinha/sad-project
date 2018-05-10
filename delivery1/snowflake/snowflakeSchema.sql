ea5bdcba532ab2847659081c581aa1713234b3d8
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-05-10 02:45:47 -0300

    Add new dimensions

--- delivery1/snowflake/snowflakeSchema.sql
+++ delivery1/snowflake/snowflakeSchema.sql
@@ -40,3 +40,67 @@ CREATE TABLE ContactDimension (
     PRIMARY KEY (contactId),
     FOREIGN KEY (customerId) REFERENCES CustomerDimension(customerId)
 );
+
+CREATE TABLE ShipperDimension (
+	shipperId INTEGER,
+	companyName VARCHAR(40),
+	PRIMARY KEY (shipperId)
+);
+
+CREATE TABLE ProductDimension (
+	productId INTEGER,
+	productDetailId INTEGER,
+	supplierId INTEGER,
+	categoryId INTEGER,
+	PRIMARY KEY (productId),
+	FOREIGN KEY (productDetailId) REFERENCES ProductDetailsDimension(productDetailId),
+	FOREIGN KEY (supplierId) REFERENCES SuppliersDimension(supplierId),
+	FOREIGN KEY (categoryId) REFERENCES CategoryDimension(categoryId)
+);
+
+CREATE TABLE ProductDetailsDimension (
+	productDetailId INTEGER,
+	productName VARCHAR(20),
+	quantityPerUnit INTEGER,
+	unitPrice INTEGER,
+	unitsInStock INTEGER,
+	unitsInOrder INTEGER,
+	reorderLevel INTEGER,
+	discontinued BIT,
+	PRIMARY KEY (productDetailId)
+);
+
+CREATE TABLE CategoryDimension (
+	categoryId INTEGER,
+	categoryName VARCHAR(10),
+	categoryDescription VARCHAR(200),
+	PRIMARY KEY (categoryId)
+);
+
+CREATE TABLE SupplierContactDimension (
+    supplierContactId INTEGER,
+    supplierContactName VARCHAR(70),
+	supplierContactTitle VARCHAR(70),
+    PRIMARY KEY (contactId),
+);
+
+CREATE TABLE SuppliersDimension (
+	supplierId INTEGER,
+	companyName VARCHAR(20),
+	supplierContactId INTEGER,
+	PRIMARY KEY (supplierId),
+	FOREIGN KEY (supplierContactId) REFERENCES SuppliersContactDimension(supplierContactId)
+);
+
+CREATE TABLE OrderFact (
+	orderId INTEGER,
+	customerId INTEGER,
+	employeeId INTEGER,
+	shipperId INTEGER,
+	productId INTEGER,
+	PRIMARY KEY (orderId),
+	FOREIGN KEY (customerId) REFERENCES CustomerDimension(customerId),
+	FOREIGN KEY (employeeId) REFERENCES EmployeeDimension(employeeId),
+	FOREIGN KEY (shipperId) REFERENCES ShipperDimension(shipperId),
+	FOREIGN KEY (productId) REFERENCES ProductDimension(productId)
+);
